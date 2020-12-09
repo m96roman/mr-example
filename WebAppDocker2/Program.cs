453a57ccb291dbda224c6593d39418e8c6390d6c
Mykhailo Roman <m96.roman@gmail.com>
2020-12-09 22:39:57 +0200

    Initial commit

---
+++ WebAppDocker2/Program.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace WebAppDocker2
+{
+    public class Program
+    {
+        public static void Main(params string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    var port = Environment.GetEnvironmentVariable("PORT");
+
+                    webBuilder
+                        .UseStartup<Startup>()
+                        .UseUrls("http://*:" + port);
+                });
+    }
+}
